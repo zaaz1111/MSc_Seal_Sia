053085cbe60346176c53566a4bb47bb295529199
zaaz1111 <zaahir11@gmail.com>
2023-06-22 19:28:52 -0400

    Installed JAGS (not in this project repo but kinda just works anyway???) and started using SIBER to plot some ellipses. Obvs will have to faff about with the burnin and other params, but feels like a good start. Just have to wait for mixSIAR ig.

--- HG_PV_Seal_Isotopes.R
+++ HG_PV_Seal_Isotopes.R
@@ -1,7 +1,7 @@
 ##Use librarian or pacman on startup to manage all packages 
 ##Figure out how to use a package on startup
 library(librarian)
-shelf(MixSIAR,SIBER,here,tidyverse,ggplot2)
+shelf(MixSIAR,SIBER,here,tidyverse,ggplot2,rjags)
 
 #Read in the data
 sidat<-read.csv(here('Processed Data/Exploratory_plate_1_Data.csv'),head=T)
@@ -15,6 +15,60 @@ sidat<-mutate(sidat,Species=case_when(
            substr(sidat$name, 6, 6) == 'P' ~ 'Plasma'
          ))%>%
   subset(sidat$C.N.ratio>2)
+
+##Doing SIBER stuff from CRAn
+siberdat<-subset(sidat,select=c("δ15N..air","δ13C...V.PDB",'Species','Type'))
+colnames(siberdat)<-c("iso2", "iso1", "community", "group")
+siberdat<-siberdat[, c(2,1,4,3)]%>%
+  createSiberObject()
+
+community.hulls.args <- list(col = 1, lty = 1, lwd = 1)
+group.ellipses.args  <- list(n = 100, p.interval = 0.95, lty = 1, lwd = 2)
+group.hulls.args     <- list(lty = 2, col = "grey20")
+
+
+plotSiberObject(siberdat,
+                ax.pad = 2, 
+                hulls = F, community.hulls.args = community.hulls.args, 
+                ellipses = T, group.ellipses.args = group.ellipses.args,
+                group.hulls = T, group.hulls.args = group.hulls.args,
+                bty = "L",
+                iso.order = c(1,2),
+                xlab = expression({delta}^13*C~'\u2030'),
+                ylab = expression({delta}^15*N~'\u2030')
+)
+
+group.ML <- groupMetricsML(siberdat)%>%
+  data.frame()
+print(group.ML)
+
+###Starting a bayesian model?!?!?
+parms<-list()
+parms$n.iter<-2*10^4
+parms$n.burnin<-1*10^3
+parms$n.thin <- 10     # thin the posterior by this many
+parms$n.chains <- 2 
+
+priors <- list()
+priors$R <- 1 * diag(2)
+priors$k <- 2
+priors$tau.mu <- 1.0E-3
+
+#Fitting the ellipses using the specified above parameters (This uses JAGS, something I am still learning)
+ellipses.posterior <- siberMVN(siberdat, parms, priors)
+
+SEA.B <- siberEllipses(ellipses.posterior)
+
+siberDensityPlot(SEA.B, xticklabels = colnames(group.ML), 
+                 xlab = c("Community | Group"),
+                 ylab = expression("Standard Ellipse Area " ('\u2030' ^2) ),
+                 bty = "L",
+                 las = 1,
+                 main = "SIBER ellipses on each group"
+)
+points(1:ncol(SEA.B), group.ML[3,], col="red", pch = "x", lwd = 2)
+
+
 
 #plot the whole thing
 ggplot(sidat)+
@@ -47,8 +101,8 @@ ggplot(sidat[sidat$Type=='RBC',])+
                level = 0.95,
                type = "norm",
                geom = "polygon") + 
-  xlab('δ13C')+
-  ylab('δ15N')+
+  xlab(expression({delta}^13*C~'\u2030'))+
+  ylab(expression({delta}^15*N~'\u2030'))+
   theme_minimal()
 
 
