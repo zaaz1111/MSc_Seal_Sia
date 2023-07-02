42c449f72772f01f328e4ed4efd543a59c263325
zaaz1111 <zaahir11@gmail.com>
2023-07-02 12:10:25 -0400

    Did a moxing model!!! It's not working though, and it's also a lot of silly goose data. Gonna need some more cleaning, bt this commit has some already

--- MSc_Seal_Fish_SIMM.R
+++ MSc_Seal_Fish_SIMM.R
@@ -1,2 +1,35 @@
 library(librarian)
-shelf(MixSIAR,SIBER,here,tidyverse,ggplot2,rjags,ellipse)
+shelf(MixSIAR,SIBER,here,tidyverse,ggplot2,rjags,ellipse,readxl)
+
+mix <- load_mix_data(filename = here('Processed Data/Consumer_mixing_data.csv'),
+                     iso_names<-c('δ13C...V.PDB', 'δ15N..air'),
+                     factors = NULL,
+                     fac_random = NULL,
+                     fac_nested = NULL,
+                     cont_effects = NULL)
+
+
+source <- load_source_data(filename = here('Processed Data/Source_mixing_data.csv'),
+                           source_factors = NULL,
+                           conc_dep = F,
+                           data_type = 'raw',
+                           mix)
+
+
+#Make discrimination data with published values from the literature
+discr <- load_discr_data(filename=here('Dummy_tdfs.csv'), mix)
+
+# Make an isospace plot
+plot_data(filename="isospace_plot", plot_save_pdf=TRUE, plot_save_png=FALSE, mix,source,discr)
+
+# Define model structure and write JAGS model file
+model_filename <- "MixSIAR_model_msc_seals_dummy_1.txt"   # Name of the JAGS model file
+resid_err <- TRUE
+process_err <- TRUE
+write_JAGS_model(model_filename, resid_err, process_err, mix, source)
+
+# Run the JAGS model ("very long" took ~5 min)
+jags.uninf <- run_model(run="test",mix,source,discr,model_filename)
+
+# Process diagnostics, summary stats, and posterior plots
+output_JAGS(jags.uninf, mix, source)
