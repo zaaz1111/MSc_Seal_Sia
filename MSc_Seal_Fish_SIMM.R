713aa5d641aa35181c0a9db424137aaf654feeb9
zaaz1111 <zaahir11@gmail.com>
2023-07-03 10:13:16 -0400

    Ran a grey seal model and realized that run='test' is not the most accurate model. Going to get huge on the modelling after some 'very short' params, but want to push everything to start thinking about virtual desktops.

--- MSc_Seal_Fish_SIMM.R
+++ MSc_Seal_Fish_SIMM.R
@@ -33,3 +33,33 @@ jags.uninf <- run_model(run="test",mix,source,discr,model_filename)
 
 # Process diagnostics, summary stats, and posterior plots
 output_JAGS(jags.uninf, mix, source)
+
+
+
+mix_harbs <- load_mix_data(filename = here('Processed Data/Consumer_harbs.csv'),
+                     iso_names<-c('δ13C...V.PDB', 'δ15N..air'),
+                     factors = NULL,
+                     fac_random = NULL,
+                     fac_nested = NULL,
+                     cont_effects = NULL)
+
+source <- load_source_data(filename = here('Processed Data/Source_mixing_data.csv'),
+                           source_factors = NULL,
+                           conc_dep = F,
+                           data_type = 'raw',
+                           mix)
+
+discr_harbs <- load_discr_data(filename=here('less_dummy_tdfs_harbs.csv'), mix)
+
+plot_data(filename="isospace_plot", plot_save_pdf=TRUE, plot_save_png=FALSE, mix_harbs,source,discr_harbs)
+
+model_filename <- "MixSIAR_model_msc_harbs_kinda_dummy_1.txt"   # Name of the JAGS model file
+resid_err <- TRUE
+process_err <- TRUE
+write_JAGS_model(model_filename, resid_err, process_err, mix, source)
+
+# Run the JAGS model ("very long" took ~5 min)
+jags.uninf_harbs <- run_model(run="test",mix_harbs,source,discr_harbs,model_filename)
+
+# Process diagnostics, summary stats, and posterior plots
+output_JAGS(jags.uninf, mix, source)
