29a0b9ebee05ebb5f972a6d2af2cef8d651403a2
zaaz1111 <zaahir11@gmail.com>
2023-07-04 14:15:34 -0400

    Ran some models and got some outputs

--- Fish_data_cleaning.R
+++ Fish_data_cleaning.R
@@ -15,7 +15,75 @@ fs<-read.csv("C:/Users/zaahi/Documents/fish.samples.csv")%>%
   ))
 
 fish.samples <- read.csv(here('fish.samples.csv'))
-fish.data <- read.csv(here('Processed Data/LE_Fish_CN_Collated.csv'))%>%
-  merge(fish.samples)%>%
-  subset(select=c(1,2,3,9,11))%>%
-  write.csv(file='Source_mixing_data.csv')
+fish.data <- read.csv(here('Processed Data/LE_Fish_CN_Collated.csv'))
+fish.merge<-merge(fish.data,fish.samples)%>%
+  subset(select=c(1,2,3,9,11))
+write.csv(fish.merge, file='Source_mixing_data.csv')
+
+
+###Compare LE and LR fish tissue
+#Load in any dfs with fish data
+#separate them by LE and LR
+#rename the columns so they have LE/LR naming conventions
+#Pull the CN ratios and add to the dfs
+#Plot clustered boxplots
+fish1 <- read_xlsx(here('Raw Data/Fish and Seal Plate Collated Raw (Plate 2).xlsx'))
+fish2 <- read_csv(here('Raw Data/Cleaned_plate_3.csv'))
+bigfish <- rbind(fish1,fish2)
+
+LR_fish <- c(fish1$name,fish2$name)
+LR_fish<-LR_fish[nchar(LR_fish)<=4]
+
+fish<-c(fish1$name[5:20],fish1$name[27:69],fish2$name[1:11])%>%
+  data.frame()
+
+colnames(fish)<-'LE_sample_name'
+fish<-fish%>%
+  mutate(LR_sample_name = case_when(
+    substr(fish$LE_sample_name,1,4) %in% LR_fish ~ substr(LE_sample_name,1,4)))
+    
+le_cn<-{}
+for(i in fish$LE_sample_name){
+  for(z in bigfish$name){
+    if(i==z){
+      le_cn<-append(le_cn,bigfish$`C/N`[bigfish$name==z])
+    }
+  }
+}
+
+fish <- cbind(fish,le_cn)
+
+lr_cn<-{}
+for(i in na.omit(fish$LR_sample_name)){
+  for(z in bigfish$name){
+    if(i==z){
+      lr_cn<-append(lr_cn,bigfish$`C/N`[bigfish$name==z])
+    }
+  }
+}
+
+index <- 1
+lr_cn_ord <- {}
+for(i in fish$LR_sample_name){
+  if(is.na(i)==F){
+    lr_cn_ord <- append(lr_cn_ord, lr_cn[index])
+    index <- index + 1
+  } else{
+    lr_cn_ord <- append(lr_cn_ord, NA)
+  }
+}
+
+fish <- cbind(fish,lr_cn_ord)
+
+spp <- {}
+for(i in fish$LE_sample_name){
+  for(z in fish.samples$name){
+    if(i==z){
+      spp<-append(spp,fish.samples$Spp[fish.samples$name==z])
+    }
+  }
+}
+  
+ish<-fish[-16,]
+fish <- cbind(fish,spp)
+
--- MSc_Seal_Fish_SIMM.R
+++ MSc_Seal_Fish_SIMM.R
@@ -1,5 +1,5 @@
 library(librarian)
-shelf(MixSIAR,SIBER,here,tidyverse,ggplot2,rjags,ellipse,readxl)
+shelf(MixSIAR,SIBER,here,tidyverse,ggplot2,rjags,ellipse,readxl,beepr)
 
 mix <- load_mix_data(filename = here('Processed Data/Consumer_mixing_data.csv'),
                      iso_names<-c('δ13C...V.PDB', 'δ15N..air'),
@@ -34,10 +34,9 @@ jags.uninf <- run_model(run="test",mix,source,discr,model_filename)
 # Process diagnostics, summary stats, and posterior plots
 output_JAGS(jags.uninf, mix, source)
 
-
-
+#Do the same model but just for harbor seals
 mix_harbs <- load_mix_data(filename = here('Processed Data/Consumer_harbs.csv'),
-                     iso_names<-c('δ13C...V.PDB', 'δ15N..air'),
+                     iso_names<-c('d13c', 'd15n'),
                      factors = NULL,
                      fac_random = NULL,
                      fac_nested = NULL,
@@ -47,19 +46,65 @@ source <- load_source_data(filename = here('Processed Data/Source_mixing_data.cs
                            source_factors = NULL,
                            conc_dep = F,
                            data_type = 'raw',
-                           mix)
+                           mix_harbs)
 
 discr_harbs <- load_discr_data(filename=here('less_dummy_tdfs_harbs.csv'), mix)
 
 plot_data(filename="isospace_plot", plot_save_pdf=TRUE, plot_save_png=FALSE, mix_harbs,source,discr_harbs)
 
-model_filename <- "MixSIAR_model_msc_harbs_kinda_dummy_1.txt"   # Name of the JAGS model file
+model_filename <- "MixSIAR_model_msc_harbs_kinda_dummy_2.txt"   # Name of the JAGS model file
 resid_err <- TRUE
 process_err <- TRUE
-write_JAGS_model(model_filename, resid_err, process_err, mix, source)
+write_JAGS_model(model_filename, resid_err, process_err, mix_harbs, source)
 
 # Run the JAGS model ("very long" took ~5 min)
-jags.uninf_harbs <- run_model(run="test",mix_harbs,source,discr_harbs,model_filename)
+jags.uninf_harbs_vl <- run_model(run="long",mix_harbs,source,discr_harbs,model_filename)
+
 
 # Process diagnostics, summary stats, and posterior plots
-output_JAGS(jags.uninf, mix, source)
+output_JAGS(jags.uninf_harbs_vl, mix_harbs, source,
+            output_options = list(summary_save = TRUE, summary_name = "summary_statistics_harbs_long",
+                                  sup_post = FALSE, plot_post_save_pdf = TRUE, plot_post_name = "posterior_density_harbs_long",
+                                  sup_pairs = FALSE, plot_pairs_save_pdf = TRUE, plot_pairs_name = "pairs_plot_harbs_long", 
+                                  sup_xy = TRUE, plot_xy_save_pdf = FALSE, plot_xy_name = "xy_plot_harbs_long", 
+                                  gelman = TRUE, heidel = FALSE, geweke = TRUE, diag_save = TRUE, diag_name = "diagnostics_harbs_long", 
+                                  indiv_effect = FALSE, plot_post_save_png = FALSE, plot_pairs_save_png = FALSE, plot_xy_save_png =
+                                  FALSE, diag_save_ggmcmc = TRUE))
+
+#Do the same model but now for grey seals
+mix_greys <- load_mix_data(filename = here('Processed Data/Consumer_grey.csv'),
+                           iso_names<-c('d13c', 'd15n'),
+                           factors = NULL,
+                           fac_random = NULL,
+                           fac_nested = NULL,
+                           cont_effects = NULL)
+
+source <- load_source_data(filename = here('Processed Data/Source_mixing_data.csv'),
+                           source_factors = NULL,
+                           conc_dep = F,
+                           data_type = 'raw',
+                           mix_greys)
+
+discr_greys <- load_discr_data(filename=here('less_dummy_tdfs_greys.csv'), mix_greys)
+
+plot_data(filename="isospace_plot_greys", plot_save_pdf=TRUE, plot_save_png=FALSE, mix_greys,source,discr_greys)
+
+model_filename <- "MixSIAR_model_msc_greys_kinda_dummy_1.txt"   # Name of the JAGS model file
+resid_err <- TRUE
+process_err <- TRUE
+write_JAGS_model(model_filename, resid_err, process_err, mix_greys, source)
+
+# Run the JAGS model ("very long" took ~5 min)
+jags.uninf_greys_l <- run_model(run="long",mix_greys,source,discr_greys,model_filename)
+
+# Process diagnostics, summary stats, and posterior plots
+output_JAGS(jags.uninf_greys_l, mix_greys, source,
+            output_options = list(summary_save = TRUE, summary_name = "summary_statistics_greys_l",
+                                  sup_post = FALSE, plot_post_save_pdf = TRUE, plot_post_name = "posterior_density_greys_l",
+                                  sup_pairs = FALSE, plot_pairs_save_pdf = TRUE, plot_pairs_name = "pairs_plot_greys_l", 
+                                  sup_xy = TRUE, plot_xy_save_pdf = FALSE, plot_xy_name = "xy_plot_greys_l", 
+                                  gelman = TRUE, heidel = FALSE, geweke = TRUE, diag_save = TRUE, diag_name = "diagnostics_greys_l", 
+                                  indiv_effect = FALSE, plot_post_save_png = FALSE, plot_pairs_save_png = FALSE, plot_xy_save_png =
+                                  FALSE, diag_save_ggmcmc = TRUE))
+#Further Split for plasma and rbc's???
+
